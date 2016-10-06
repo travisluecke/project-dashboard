c7e664500688463948211436502847a0863df60a
Willy Connor <willyconnor@gmail.com>
2016-10-05 23:38:05 -0400

    getting to know the project :)

--- src/main/java/ztco/Application.java
+++ src/main/java/ztco/Application.java
@@ -2,15 +2,24 @@ package ztco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import java.util.Arrays;
  
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        System.out.println("Debug Info inspect Beans provided by Spring Boot");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for(String beanName : beanNames){
+            System.out.println(beanName);
+        }
     }
     
 }
